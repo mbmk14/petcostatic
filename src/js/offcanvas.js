6778574267791fd14acb67a42a8a2751d37ebe0f
Prafull Sharma <prafull.sharma@ekino.com>
2018-06-27 15:01:11 +0530

    progress

--- src/js/offcanvas.js
+++ src/js/offcanvas.js
@@ -3,6 +3,19 @@ $(function () {
   'use strict'
 
   $('[data-toggle="offcanvas"]').on('click', function () {
-    $('.offcanvas-collapse').toggleClass('open')
-  })
+    $('.offcanvas-collapse').addClass('open')
+  });
+
+  $('.offcanvas-collapse-close').on('click', function () {
+    $('.offcanvas-collapse').removeClass('open')
+  });
+
+  let windowWidth = $(document).innerWidth();
+
+  if(windowWidth < 992) (
+    $('.nav-link.top-level').on('click', function () {
+      $(this).toggleClass('active').next().toggleClass('open')
+    })
+  )
+
 })
